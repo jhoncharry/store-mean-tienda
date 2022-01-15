509c291961154f45126be250c4eadb37def680c3
Jhon <jhon.charryserrato@gmail.com>
2022-01-14 23:26:16 -0500

    Preparing for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:3000/api/',
-  socket: 'http://localhost:3000',
+  api: 'https://store-mean.herokuapp.com/api/',
+  socket: 'https://store-mean.herokuapp.com',
   stripe_pk:
     'pk_test_51KF5QELFWDaOAjUBSmlSzkno5MEKW6XmTKQVLhTeKRiDNTyUa8RmnAuM4IUfcrQbX4KDFcohGbGYOwv8MwFkO5hb00ElHOCLQI',
 };
